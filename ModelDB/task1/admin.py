59e7c518f07beb313f29c79e9b7d9bc91e31cb5f
Vladimir Medvedev <vmvlad84@gmail.com>
2024-12-26 21:01:32 +0300

    Сайт администрирования

--- ModelDB/task1/admin.py
+++ ModelDB/task1/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Buyer, Game
 
-admin.site.register(Buyer)
-admin.site.register(Game)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',)
+    search_fields = ('title',)
+    list_filter = ('size', 'cost',)
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age',)
+    search_fields = ('name',)
+    list_filter = ('balance', 'age',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
